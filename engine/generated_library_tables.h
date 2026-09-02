43b6241ab0497003eabafd9e14f931e672321c69
Gerardo-Hub17 <gjdelgadogarcia@gmail.com>
2026-09-01 23:44:41 -0500

    Add GX context, vid_ogc, static exports wiring and CMake ref_gx

--- engine/generated_library_tables.h
+++ engine/generated_library_tables.h
@@ -2,13 +2,13 @@
 
 extern table_t lib_filesystem_stdio_exports[];
 extern table_t lib_hl_exports[]; //hl_dll
-extern table_t lib_ref_soft_exports[];
+extern table_t lib_ref_gx_exports[]; //ref_gx
 extern table_t lib_cl_dll_exports[]; //cl_dll
 extern table_t lib_menu_exports[];
 
 struct {const char *name;void *func;} libs[] = {
 { "filesystem_stdio", &lib_filesystem_stdio_exports },
-{ "ref_soft", &lib_ref_soft_exports },
+{ "ref_gx", &lib_ref_gx_exports },
 { "server", &lib_hl_exports },
 { "client", &lib_cl_dll_exports },
 { "menu", &lib_menu_exports},
